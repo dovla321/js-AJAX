7feeef40c30a9cb65a71ddb62791ebef1a9b1b65
spider4321 <fit1991forever@gmail.com>
2022-06-14 02:31:55 -1200

    js ajax fetch

--- main.js
+++ main.js
@@ -8,19 +8,16 @@ b.innerHTML = "<p>ovo je recenica</p>"; //3
 
 //  https://62a88cb0943591102ba4e94b.mockapi.io
 
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-	if(this.readyState == 4 && this.status == 200){
-		 console.log(xhttp.responseText);
-	}
-}
-
-xhttp.open('GET', 'https://62a88cb0943591102ba4e94b.mockapi.io/Projekt1', true);
-xhttp.send();
+let r = fetch('https://62a88cb0943591102ba4e94b.mockapi.io/Projekt1')
+.then(response => response.json()).then(data => {
+	console.log(data);
+}).catch(error => {
+	alert(error);
+});
 
 
 
-document.write("pisanje");  //4
+
 
 /*let t = document.querySelector('body p');
 
