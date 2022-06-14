11b350e67b2b2e439310fa8d716c43d753084eb8
spider4321 <fit1991forever@gmail.com>
2022-06-14 02:11:59 -1200

    treći update js AJAX

--- main.js
+++ main.js
@@ -4,15 +4,24 @@ console.log('test'); //1
 
 let b =document.querySelector('body'); //2
 
-setTimeout(function () {
+b.innerHTML = "<p>ovo je recenica</p>"; //3
 
-},5000);
+//  https://62a88cb0943591102ba4e94b.mockapi.io
+
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+	if(this.readyState == 4 && this.status == 200){
+		 console.log(xhttp.responseText);
+	}
+}
+
+xhttp.open('GET', 'https://62a88cb0943591102ba4e94b.mockapi.io/Projekt1', true);
+xhttp.send();
 
 
-/*b.innerHTML = "<p>ovo je recenica</p>"; //3
 
 document.write("pisanje");  //4
 
-let t = document.querySelector('body p');
+/*let t = document.querySelector('body p');
 
 t.style.color = 'red';*/
