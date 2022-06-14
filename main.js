1f89171a0f4a26ddd19f69777f27565d2d0957d5
spider4321 <fit1991forever@gmail.com>
2022-06-14 01:17:22 -1200

    druga js izmjena

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log('test'); //1
+
+
+
+let b =document.querySelector('body'); //2
+
+setTimeout(function () {
+
+},5000);
+
+
+/*b.innerHTML = "<p>ovo je recenica</p>"; //3
+
+document.write("pisanje");  //4
+
+let t = document.querySelector('body p');
+
+t.style.color = 'red';*/
